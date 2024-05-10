fa3df2b58d71288857ed79e44a9d12e3cc8f52f2
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-10 07:25:03 -0700

    Module1Project Node db

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,52 +2,53 @@ const Account = require('./accounts-model')
 const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const error = {status:400}
-  const {name, budget} = req.body
-  if(name === undefined || budget === undefined){
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined) {
     error.message = 'name and budget are required'
-  }else if(typeof name !== 'string'){
+  } else if (typeof name !== 'string') {
     error.message = 'name of account must be a string'
-  }else if(name.trim().length < 3 || name.trim().length > 100){
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
     error.message = 'name of account must be between 3 and 100'
-  }else if(typeof budget !== 'number' || isNaN(budget)){
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
     error.message = 'budget of account must be a number'
-  }else if(budget<1 || budget > 1000000){
+  } else if (budget < 1 || budget > 1000000) {
     error.message = 'budget of account is too large or too small'
   }
 
-  if(error.message){
+  if (error.message) {
     next(error)
-  }else{
+  } else {
     next()
   }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
-  try{
+  try {
     const existing = await db('accounts')
-    .where('name', req.body.name.trim())
-    .first()
+      .where('name', req.body.name.trim())
+      .first()
 
-    if(existing){
-      next({message:'that name is taken', status:400})
-    }else{
+    if (existing) {
+      next({ message: 'that name is taken', status: 400 })
+    } else {
       next()
     }
-  }catch(err){
+  } catch (err) {
     next(err)
   }
 }
 
 exports.checkAccountId = async (req, res, next) => {
-  try{
+  try {
     const account = await Account.getById(req.params.id)
-    if(!account){
-      next({status:404, message: "account not found"})
-    }else{
-  req.account = account
-  next()    }
-  }catch(err){
+    if (!account) {
+      next({ status: 404, message: "account not found" })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
     next(err)
   }
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,27 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
- return db('accounts')
+  return db('accounts')
 }
 
 const getById = id => {
   return db('accounts').where('id', id).first()
 }
 
-const create =async account => {
-  const [id] =await db('accounts').insert(account)  
-   return getById(id)
+const create = async account => {
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById =async (id, account) => {
+const updateById = async (id, account) => {
   await db('accounts')
-  .where('id', id)
-  .update(account)
+    .where('id', id)
+    .update(account)
   return getById(id)
 }
 
 const deleteById = id => {
-  return db('accounts').where('id',id).del()
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
